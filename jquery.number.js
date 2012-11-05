7cda28c34f6b53375b3a0febbf0e6ac09d516dd9
Sam Sehnert <sam@teamdf.com>
2012-11-06 09:57:41 +1300

    Initial version one commit
    Original version of the number format plugin

---
+++ jquery.number.js
@@ -0,0 +1,32 @@
+(function($){
+	
+	/**
+	 * A quick plugin which implements phpjs.org's number_format as a jQuery
+	 * plugin which formats the number, and inserts to the DOM.
+	 *
+	 * By Sam Sehnert, teamdf.com — http://www.teamdf.com/web/jquery-number-format/178/
+	 */
+	$.fn.number = function( number, decimals, dec_point, thousands_sep ){	
+	    number = (number + '').replace(/[^0-9+\-Ee.]/g, '');
+	    var n = !isFinite(+number) ? 0 : +number,
+	        prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+	        sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
+	        dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
+	        s = '',
+	        toFixedFix = function (n, prec) {
+	            var k = Math.pow(10, prec);
+	            return '' + Math.round(n * k) / k;
+	        };
+	    // Fix for IE parseFloat(0.55).toFixed(0) = 0;
+	    s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
+	    if (s[0].length > 3) {
+	        s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+	    }
+	    if ((s[1] || '').length < prec) {
+	        s[1] = s[1] || '';
+	        s[1] += new Array(prec - s[1].length + 1).join('0');
+	    }
+	    // Add this number to the element as text.
+	    this.text( s.join(dec) );
+	};
+})(jQuery);
