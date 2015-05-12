c2740f9f6dcae9166742e14cd084ac58d3bd389d
Indri Muska <indrimuska@gmail.com>
2015-05-12 20:04:30 +0200

    Fix rounding bug for number with exponential part
    When a number presents an exponential part (scientific representation,
    ie. 5e+3) an additional “e+” string is appended to the number to
    perform the `toFixedFix` function. It causes the `Math.round` method to
    return a `NaN` value.

--- jquery.number.js
+++ jquery.number.js
@@ -746,7 +746,7 @@
 		var n = !isFinite(+number) ? 0 : +number,
 			s = '',
 			toFixedFix = function (n, decimals) {
-				return '' + (+(Math.round(n + 'e+' + decimals) + 'e-' + decimals));
+				return '' + (+(Math.round(('' + n).indexOf('e') > 0 ? n : n + 'e+' + decimals) + 'e-' + decimals));
 			};
 
 		// Fix for IE parseFloat(0.55).toFixed(0) = 0;
