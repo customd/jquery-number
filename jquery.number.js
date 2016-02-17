94dbc68d2deb7851c2f37608a18a27bc1fa46e31
kztime <tdp.tft@gmail.com>
2016-02-17 16:18:57 +0600

    Fix bug when type another numeric symbol from some android phones (Samsung)

--- jquery.number.js
+++ jquery.number.js
@@ -228,6 +228,7 @@
 								( (key >= 35 && key <= 39) ) ||
 								// Allow: F1-F12
 								( (key >= 112 && key <= 123) )
+								(  key == 229 )
 							){
 								return;
 							}
@@ -476,7 +477,16 @@
 						}
 
 						// Stop executing if the user didn't type a number key, a decimal, or a comma.
-						if( this.value === '' || (code < 48 || code > 57) && (code < 96 || code > 105 ) && code !== 8 && code !== 46 && code !== 110 ) return;
+						if (this.value === '' ||
+								(code < 48 || code > 57) &&
+								(code < 96 || code > 105 ) &&
+								code !== 8 &&
+								code !== 46 &&
+								code !== 110 &&
+								code !== 229
+						) {
+							return;
+						}
 
 						// Re-format the textarea.
 						$this.val($this.val());
